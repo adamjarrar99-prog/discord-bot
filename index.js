9899afc66b7d13ea70e3fae55593d573de958a5c
adamjarrar99-prog <adamjarrar99@gmail.com>
2026-04-13 13:35:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates]
 });
 
-const CHANNEL_ID = 850155403349065751;
-const GUILD_ID = 850155402141237318;
+const CHANNEL_ID = '850155403349065751';
+const GUILD_ID = '850155402141237318';
 const VIDEO_URL = "https://www.youtube.com/watch?v=i46lq8NS2JI";
 
 client.once('ready', async () => {
